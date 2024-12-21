562aed7274d67746a51c7cabf7ccb82ec80d4a60
vincentngo-nvd <ngovanduong.a2@gmail.com>
2024-12-21 09:16:57 +0700

    Push code ban đầu của project

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KTCK_124LTTD04_Nhom16"
+include(":app")
+ 
